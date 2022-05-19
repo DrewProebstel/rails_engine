1ea8327018907a21a46b480df46bb441f5f2b76b
Drew Proebstel <d.proebstel@gmail.com>
2022-05-18 18:46:58 -0700

    feature: add items index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'api/v1/merchants/:id/items', to: 'api/v1/items#index'
+  # get 'api/v1/merchants/:id/items', to: 'api/v1/items#index'
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
-
+      resources :items, only: [:index, :show] do
+      end
+      resources :merchants, only: [:index, :show] do
+        resources :items, controller: 'merchants_items', action: :index
+      end
     end
   end
 end
